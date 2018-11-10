a2d236b0b44272e2a285d02411723f108c534557
Akira Kamite <akirakamite@akiranomacbook-puro.local>
2018-11-10 19:21:36 +0900

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by Akira Kamite on 2018/11/10.
+//  Copyright © 2018 araki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
